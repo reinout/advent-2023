f58fd6a0a6988ca0919a634318cbe02230a36a45
Reinout van Rees <reinout@vanrees.org>
2023-12-06 10:23:28 +0100

    Added type hints

--- day01/script.py
+++ day01/script.py
@@ -1,14 +1,14 @@
 import pathlib
 
 
-def line_solution(line):
+def line_solution(line: str) -> int:
     numbers = [character for character in line if character in "0123456789"]
     first = numbers[0]
     last = numbers[-1]
     return int(first + last)
 
 
-def solution(content):
+def solution(content: str) -> int:
     lines = [line for line in content.split("\n") if line]
     return sum([line_solution(line) for line in lines])
 
