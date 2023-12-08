7a2af6a4bc9f994ce62deb9bb8cc27b4a4c44355
Reinout van Rees <reinout@vanrees.org>
2023-12-08 22:49:32 +0100

    Some more functionality

--- src/day03/day03a.py
+++ src/day03/day03a.py
@@ -1,6 +1,57 @@
-def text_to_padded_lines(text: str) -> list[str]:
+from dataclasses import dataclass
+
+
+@dataclass
+class Segment:
+    """Line number and character number from/to, 0-based
+
+    The 'end' is inclusive, btw.
+
+    """
+
+    line: int
+    start: int
+    end: int
+
+
+@dataclass
+class Box:
+    """Box coordinates around characters.
+
+    Character 0 on line 0 has box coordinates 0,0 (top left) to 1,1 (bottom right).
+
+    """
+
+    left: int
+    right: int
+    top: int
+    bottom: int
+
+
+@dataclass
+class Matrix:
+    lines: list[str]
+
+
+def text_to_padded_matrix(text: str) -> Matrix:
     """Return text splitted into lines, but padded on all sides with dots"""
     lines = [line for line in text.split("\n") if line.strip()]
     len_line = len(lines[0])
     dots = "." * len_line
-    return ["." + line + "." for line in [dots] + lines + [dots]]
+    padded_lines = ["." + line + "." for line in [dots] + lines + [dots]]
+    return Matrix(lines=padded_lines)
+
+
+def segment_to_box(segment: Segment) -> Box:
+    top = segment.line
+    bottom = segment.line + 1
+    left = segment.start
+    right = segment.end + 1
+    return Box(top=top, bottom=bottom, left=left, right=right)
+
+
+def box_with_padding(box: Box) -> Box:
+    """Return box, padded on all sides by one character"""
+    return Box(
+        top=box.top - 1, bottom=box.bottom + 1, left=box.left - 1, right=box.right + 1
+    )
--- src/day03/test_day03a.py
+++ src/day03/test_day03a.py
@@ -1,4 +1,4 @@
-from day03.day03a import text_to_padded_lines
+from day03.day03a import Segment, box_with_padding, segment_to_box, text_to_padded_matrix
 
 
 TESTDATA = """\
@@ -16,5 +16,23 @@ TESTDATA = """\
 
 
 def test_text_to_padded_lines1():
-    padded_lines = text_to_padded_lines(TESTDATA)
-    assert len(padded_lines) == 1 + 10 + 1
+    matrix = text_to_padded_matrix(TESTDATA)
+    assert len(matrix.lines) == 1 + 10 + 1
+
+
+def test_segment_to_box1():
+    segment = Segment(line=2, start=1, end=3)
+    box = segment_to_box(segment)
+    assert box.top == 2
+    assert box.bottom == 3
+    assert box.left == 1
+    assert box.right == 4
+
+def test_padded_box1():
+    segment = Segment(line=2, start=1, end=3)
+    box = segment_to_box(segment)
+    padded_box = box_with_padding(box)
+    assert padded_box.top == 1
+    assert padded_box.bottom == 4
+    assert padded_box.left == 0
+    assert padded_box.right == 5
