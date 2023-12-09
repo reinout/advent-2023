1219e7d795081c6f4ba650d6880392c1a4408d62
Reinout van Rees <reinout@vanrees.org>
2023-12-09 18:23:23 +0100

    Got part 1 of day 3 done

--- src/day03/day03a.py
+++ src/day03/day03a.py
@@ -1,4 +1,8 @@
 from dataclasses import dataclass
+import pathlib
+
+NUMBERS = "0123456789"
+SYMBOLS = "!@#$%^&*()_+/=-"
 
 
 @dataclass
@@ -9,9 +13,10 @@ class Segment:
 
     """
 
-    line: int
+    line_number: int
     start: int
     end: int
+    value: int
 
 
 @dataclass
@@ -43,8 +48,8 @@ def text_to_padded_matrix(text: str) -> Matrix:
 
 
 def segment_to_box(segment: Segment) -> Box:
-    top = segment.line
-    bottom = segment.line + 1
+    top = segment.line_number
+    bottom = segment.line_number + 1
     left = segment.start
     right = segment.end + 1
     return Box(top=top, bottom=bottom, left=left, right=right)
@@ -55,3 +60,72 @@ def box_with_padding(box: Box) -> Box:
     return Box(
         top=box.top - 1, bottom=box.bottom + 1, left=box.left - 1, right=box.right + 1
     )
+
+
+def find_segments(matrix: Matrix) -> list[Segment]:
+    segments: list[Segment] = []
+    for line_number, line in enumerate(matrix.lines):
+        found = ""
+        start = 0
+        end = 0
+        for position, character in enumerate(line):
+            # Note: line always starts and ends with "."!
+            if character in NUMBERS:
+                if not found:
+                    start = position
+                found += character
+                end = position
+            else:
+                if found:
+                    # End the segment
+                    segment = Segment(
+                        line_number=line_number, start=start, end=end, value=int(found)
+                    )
+                    segments.append(segment)
+                    found = ""
+    return segments
+
+
+def characters_in_box(matrix: Matrix, box: Box) -> str:
+    result: str = ""
+    for line_number, line in enumerate(matrix.lines):
+        if line_number < box.top or line_number > (box.bottom - 1):
+            continue
+        for position, character in enumerate(line):
+            if position < box.left or position > (box.right - 1):
+                continue
+            result += character
+    return result
+
+
+def is_valid_partnumber(matrix: Matrix, segment: Segment) -> bool:
+    box = box_with_padding(segment_to_box(segment))
+    for character in characters_in_box(matrix, box):
+        if character in SYMBOLS:
+            print(f"{segment.value}: {characters_in_box(matrix, box)}")
+            return True
+    return False
+
+
+def sum_partnumbers(matrix: Matrix) -> int:
+    segments = find_segments(matrix)
+    valid_segments = [
+        segment for segment in segments if is_valid_partnumber(matrix, segment)
+    ]
+    return sum([segment.value for segment in valid_segments])
+
+
+def check_missing_symbols(matrix: Matrix):
+    for line in matrix.lines:
+        for character in line:
+            if character not in ("." + NUMBERS + SYMBOLS):
+                raise ValueError(f"Missing symbol: {character}")
+
+
+if __name__ == "__main__":
+    source = pathlib.Path("src/day03/input.txt")
+    contents = source.read_text()
+    matrix = text_to_padded_matrix(contents)
+    print("\n".join(matrix.lines))
+    check_missing_symbols(matrix)
+    print(sum_partnumbers(matrix))
--- src/day03/test_day03a.py
+++ src/day03/test_day03a.py
@@ -1,4 +1,14 @@
-from day03.day03a import Segment, box_with_padding, segment_to_box, text_to_padded_matrix
+from day03.day03a import (
+    Box,
+    Segment,
+    box_with_padding,
+    characters_in_box,
+    find_segments,
+    is_valid_partnumber,
+    segment_to_box,
+    sum_partnumbers,
+    text_to_padded_matrix,
+)
 
 
 TESTDATA = """\
@@ -15,24 +25,89 @@ TESTDATA = """\
 """
 
 
+MORE_TESTDATA = """\
+12.......*..
++.........34
+.......-12..
+..78........
+..*....60...
+78.........9
+.5.....23..$
+8...90*12...
+............
+2.2......12.
+.*.........*
+1.1..503+.56
+"""
+
+
 def test_text_to_padded_lines1():
     matrix = text_to_padded_matrix(TESTDATA)
     assert len(matrix.lines) == 1 + 10 + 1
 
 
 def test_segment_to_box1():
-    segment = Segment(line=2, start=1, end=3)
+    segment = Segment(line_number=2, start=1, end=3, value=123)
     box = segment_to_box(segment)
     assert box.top == 2
     assert box.bottom == 3
     assert box.left == 1
     assert box.right == 4
 
+
 def test_padded_box1():
-    segment = Segment(line=2, start=1, end=3)
+    segment = Segment(line_number=2, start=1, end=3, value=123)
     box = segment_to_box(segment)
     padded_box = box_with_padding(box)
     assert padded_box.top == 1
     assert padded_box.bottom == 4
     assert padded_box.left == 0
     assert padded_box.right == 5
+
+
+def test_find_segments1():
+    matrix = text_to_padded_matrix(TESTDATA)
+    segments = find_segments(matrix)
+    assert len(segments) == 10
+
+
+def test_find_segments2():
+    matrix = text_to_padded_matrix(TESTDATA)
+    segments = find_segments(matrix)
+    last_one = segments[-1]
+    assert last_one.value == 598
+    # For the tests below: remember there's 1 cell of padding everywhere!
+    assert last_one.line_number == 10
+    assert last_one.start == 6
+    assert last_one.end == 8  # inclusive, btw
+
+
+def test_characters_in_box():
+    matrix = text_to_padded_matrix(TESTDATA)
+    box = Box(top=1, bottom=4, left=1, right=4)
+    result = characters_in_box(matrix, box)
+    assert result == "467.....3"
+
+
+def test_is_valid_partnumber1():
+    matrix = text_to_padded_matrix(TESTDATA)
+    segments = find_segments(matrix)
+    valid_one = segments[0]
+    assert is_valid_partnumber(matrix, valid_one)
+
+
+def test_is_valid_partnumber2():
+    matrix = text_to_padded_matrix(TESTDATA)
+    segments = find_segments(matrix)
+    invalid_one = segments[1]
+    assert not is_valid_partnumber(matrix, invalid_one)
+
+
+def test_sum_partnumbers():
+    matrix = text_to_padded_matrix(TESTDATA)
+    assert sum_partnumbers(matrix) == 4361
+
+
+def test_sum_partnumbers2():
+    matrix = text_to_padded_matrix(MORE_TESTDATA)
+    assert sum_partnumbers(matrix) == 925
